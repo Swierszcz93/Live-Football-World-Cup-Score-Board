c7d9d1b2bad99d72e0e90120a24eb70b5bdf7455
Swierszcz93 <cyberkot93@gmail.com>
2023-12-25 11:22:45 +0100

    showcase improvements

--- main/src/main/java/pl/code/Main.java
+++ main/src/main/java/pl/code/Main.java
@@ -4,16 +4,19 @@ import pl.code.library.Scoreboard;
 
 import java.util.List;
 import java.util.Random;
+import java.util.function.Supplier;
 
 public class Main {
     private static final List<List<String>> TEAM_PAIRS = List.of(
             List.of("Germany", "France"),
             List.of("Italy", "Andora"),
             List.of("Poland", "Spain"),
-            List.of("Brazil", "Argentina")
+            List.of("Brazil", "Argentina"),
+            List.of("   ", "Empty")
     );
+    private static final int HOME = 0;
+    private static final int AWAY = 1;
     private static final int MAX_SCORE = 10;
-
     public static void main(String[] args) {
         System.out.println("Showcase started");
 
@@ -21,28 +24,39 @@ public class Main {
         System.out.println("Initial scoreboard state");
         System.out.println(scoreboard.getVizualizedString());
         for (List<String> pair : TEAM_PAIRS) {
-            System.out.printf("Starting game: %s - %s%n", pair.get(0), pair.get(1));
-            scoreboard.startGame(pair.get(0), pair.get(1));
+            System.out.printf("Starting game: %s - %s%n", pair.get(HOME), pair.get(AWAY));
+            showResult(() -> scoreboard.startGame(pair.get(HOME), pair.get(AWAY)));
         }
         System.out.println("Current scoreboard state");
         System.out.println(scoreboard.getVizualizedString());
+
+
         Random rnd = new Random();
         for (List<String> pair : TEAM_PAIRS) {
-            System.out.printf("Setting up game score: %s - %s%n", pair.get(0), pair.get(1));
-            scoreboard.updateGame(pair.get(0), pair.get(1), rnd.nextInt(MAX_SCORE), rnd.nextInt(MAX_SCORE));
+            int homeScore = rnd.nextInt(MAX_SCORE);
+            int awayScore = rnd.nextInt(MAX_SCORE);
+            System.out.printf("Setting up game score: %s %d - %s %d%n", pair.get(HOME), homeScore, pair.get(AWAY), awayScore);
+            showResult(() -> scoreboard.updateGame(pair.get(HOME), pair.get(AWAY), homeScore, awayScore));
         }
         System.out.println("Current scoreboard state");
         System.out.println(scoreboard.getVizualizedString());
+
 
         System.out.println("Removing random game");
         int gameNumber = rnd.nextInt(TEAM_PAIRS.size());
-
-        System.out.printf("Removing : %s - %s%n", TEAM_PAIRS.get(gameNumber).get(0), TEAM_PAIRS.get(gameNumber).get(1));
-        scoreboard.finishGame(TEAM_PAIRS.get(gameNumber).get(0), TEAM_PAIRS.get(gameNumber).get(1));
-
+        System.out.printf("Removing : %s - %s%n", TEAM_PAIRS.get(gameNumber).get(HOME), TEAM_PAIRS.get(gameNumber).get(AWAY));
+        showResult(() -> scoreboard.finishGame(TEAM_PAIRS.get(gameNumber).get(HOME), TEAM_PAIRS.get(gameNumber).get(AWAY)));
         System.out.println("Current scoreboard state");
         System.out.println(scoreboard.getVizualizedString());
 
         System.out.println("Showcase ended");
+    }
+
+    private static void showResult(Supplier<Boolean> function) {
+        if (function.get()) {
+            System.out.println("Success!");
+        } else {
+            System.out.println("Failure!");
+        }
     }
 }
